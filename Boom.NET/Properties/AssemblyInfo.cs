5f9c70688ecc75ac9b8291a6ba490fe538f274be
chrisinajar <chris@eazeup.com>
2015-10-14 17:53:10 -0700

    Update metadata from Visual Studio. Packagable now.

---
+++ Boom.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿/******************************************************************************
+ * $Id:  $
+ *
+ * Name:     AssemblyInfo.cs
+ * Project:  Boom.NET
+ * Purpose:  Boom.NET C# Assembly Information.
+ * Author:   Chris Vickery, chrisinajar@gmail.com
+ *
+ ******************************************************************************/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Boom.NET")]
+[assembly: AssemblyDescription("Developer friend HTTP exceptions based on hapijs/boom")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Boom.Properties")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a27bfabc-7bec-4fa3-9cb2-4195e6ce605a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
